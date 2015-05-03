b3f6d874cbf37cba4d8367384c4609d68b989d72
olya-d <inremajor@gmail.com>
2015-05-03 13:53:06 +0200

    Read voters from csv

--- Main.hs
+++ Main.hs
@@ -1,18 +1,31 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeSynonymInstances, FlexibleInstances #-}
 
 import System.Environment (getArgs)
 import Control.Applicative
 import qualified Data.ByteString.Lazy as BL
+import qualified Data.ByteString.Char8 as BC
+import qualified Data.Text as T
 import Data.Csv
 import qualified Data.Vector as V
 
 data District = District
-    { identifier   :: !String
+    { district_identifier :: !String
     , number_of_seats :: !Int
     }
 
+type Preferences = [String]
+type Probabilities = [Float]
+
+data Voter = Voter
+    { voter_identifier :: !String
+    , district :: !String
+    , preferences :: Preferences
+    , probabilities :: Probabilities
+    }
+
 instance FromNamedRecord District where
-    parseNamedRecord r = District <$> r .: "identifier" <*> r .: "number_of_seats"
+    parseNamedRecord r = District <$> r .: "district_identifier" <*> r .: "number_of_seats"
 
 readDistricts :: FilePath -> IO()
 readDistricts path = do
@@ -20,11 +33,40 @@ readDistricts path = do
     case decodeByName csvData of
         Left err -> putStrLn err
         Right (_, v) -> V.forM_ v $ \ p ->
-            putStrLn $ identifier p ++ " contributes " ++ show (number_of_seats p) ++ " seats"
+            putStrLn $ district_identifier p ++ " contributes " ++ show (number_of_seats p) ++ " seats"
+
+instance FromField Preferences where
+    parseField s = 
+        pure ((map T.unpack (T.splitOn (T.pack ":") (T.pack c))) :: Preferences)
+        where
+            c = BC.unpack s
+
+instance FromField Probabilities where
+    parseField s = 
+        pure ((map (read . T.unpack) (T.splitOn (T.pack ":") (T.pack c))) :: Probabilities)
+        where
+            c = BC.unpack s
+
+instance FromNamedRecord Voter where
+    parseNamedRecord r = Voter <$> 
+        r .: "voter_identifier" <*>
+        r .: "district" <*>
+        r .: "preferences" <*>
+        r .: "probabilities"
+
+readVoters :: FilePath -> IO()
+readVoters path = do
+    csvData <- BL.readFile path
+    case decodeByName csvData of
+        Left err -> putStrLn err
+        Right (_, v) -> V.forM_ v $ \ p ->
+            putStrLn $ voter_identifier p ++ " in district " ++ (district p) ++ " has preferences " ++ show (preferences p) ++ " and probabilities " ++ show (probabilities p)
 
 main :: IO()
 main = do
     args <- getArgs
     case args of 
-        [file] -> readDistricts file
+        [dfile, vfile] -> do
+            readDistricts dfile
+            readVoters vfile
         _ -> putStrLn "Wrong number of arguments"
