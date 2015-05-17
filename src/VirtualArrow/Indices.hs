395b7fb6899cd811013ed6c870839898808dba79
olya-d <inremajor@gmail.com>
2015-05-17 19:49:20 +0200

    Add index of governability

--- src/VirtualArrow/Indices.hs
+++ src/VirtualArrow/Indices.hs
@@ -6,6 +6,7 @@ Description: Definitions of indices of proportionality and governability.
 module VirtualArrow.Indices
 (
     gallagherIndex,
+    governability
 )
 where
 
@@ -13,8 +14,12 @@ import VirtualArrow.Input
 import VirtualArrow.Election (oneDistrictProportionality)
 import qualified Data.Map.Strict as Map
 import Control.Arrow
+import Data.List (groupBy, sortBy)
+import Data.Function (on)
 
--- | Calculates <http://en.wikipedia.org/wiki/Gallagher_Index Gallagher Index>
+import Debug.Trace
+
+-- | Computes <http://en.wikipedia.org/wiki/Gallagher_Index Gallagher Index>
 gallagherIndex :: Input -> Parliament -> Maybe (Map.Map Int Party)-> Double
 gallagherIndex input parliament candidates =
     sqrt (1.0/2.0 * fromIntegral s) / 
@@ -34,24 +39,40 @@ gallagherIndex input parliament candidates =
                     (oneDistrictProportionality input)
             Nothing -> oneDistrictProportionality input
 
--- governability :: Parliament -> [(Party, Int)] -> Double
--- governability parliament coalitions =
---     1.0/(m + 1.0) + (1.0/m - 1.0/(m + 1.0))*(f - t/2.0)/t/2.0
---   where
---     numberOfSeats = sum (map snd parliament)
---     coalitionMap = Map.fromList coalitions
---     parliamentMap = Map.fromList parliament
---     majorityCoalition =
---         maximumBy (sum $ map (parliamentMap Map.!))
---             (map 
---                 (\list@(p:_) -> coalitionMap Map.! p, list)
---                 (groupBy snd coalitions)
---             )
---     numberOfMajorParties =
---         if coalitionSeats * 2 > numberOfSeats
---             then length (filter (\p -> parliamentMap Map.! p * 2 > coalitionSeats))
---             else 0
---     coalitionSeats = sum $ map (parliamentMap Map.!) majorityCoalition
---     m = fromIntegral numberOfMajorParties
---     f = fromIntegral coalitionSeats
---     t = fromIntegral numberOfSeats
+
+-- | Computes index of governability as described in 
+-- <http://jasss.soc.surrey.ac.uk/7/3/3.html#1.22>
+governability :: Parliament 
+              -> [(Party, Int)] -- ^ assignment of party to coalition
+              -> Double
+governability parliament coalitions =
+    1.0/(m + 1.0) + (1.0/m - 1.0/(m + 1.0))*(f - t/2.0)/(t/2.0)
+  where
+    numberOfSeats :: Int
+    numberOfSeats = sum (map snd parliament)
+    coalitionMap :: Map.Map Party Int
+    coalitionMap = Map.fromList coalitions
+    parliamentMap :: Map.Map Party NumberOfSeats
+    parliamentMap = Map.fromList parliament
+    (majorityCoalitionSeats, majorityCoalition) =
+        maximum $ zip
+            (map
+                numberOfSeatsInCoalition
+                (groupBy ((==) `on` snd) (sortBy (compare `on` snd) coalitions))
+            )
+            [0..]
+    numberOfSeatsInCoalition :: [(Party, Int)] -> Int
+    numberOfSeatsInCoalition coalition =
+        sum $ map (\(p, _) -> parliamentMap Map.! p) coalition
+    numberOfMajorParties :: Int
+    numberOfMajorParties =
+        if majorityCoalitionSeats * 2 > numberOfSeats then
+            length (filter destroysMajority parliament)
+        else 0
+    destroysMajority :: (Party, NumberOfSeats) -> Bool
+    destroysMajority (p, seats) =
+        coalitionMap Map.! p == majorityCoalition && 
+        2*(majorityCoalitionSeats - seats) < numberOfSeats
+    m = fromIntegral numberOfMajorParties
+    f = fromIntegral majorityCoalitionSeats
+    t = fromIntegral numberOfSeats
--- test/VirtualArrow/IndicesSpec.hs
+++ test/VirtualArrow/IndicesSpec.hs
@@ -1,7 +1,7 @@
 module VirtualArrow.IndicesSpec (main, spec) where
 
 import VirtualArrow.Factory
-import VirtualArrow.Indices (gallagherIndex)
+import VirtualArrow.Indices
 import Test.Hspec
 
 
@@ -16,6 +16,11 @@ spec = do
             `shouldBe`
             0.0
 
-        it "should return the index of gallagherIndex" $ do
+        it "should return the Gallagher Index" $ do
             gallagherIndex input bordaCountResult Nothing `shouldBe` bordaCountR
             gallagherIndex input mixedMember2Result Nothing `shouldBe` mixedMember2R
+
+    describe "VirtualArrow.Indices.governability" $
+        it "should return the index of governability" $
+            governability [(0, 59), (1, 20), (2, 21)] [(0, 0), (1, 1), (2, 1)]
+            `shouldBe` 0.59
